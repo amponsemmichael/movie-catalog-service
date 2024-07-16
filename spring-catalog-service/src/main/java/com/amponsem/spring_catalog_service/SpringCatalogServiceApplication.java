57daedcd75efa384d0116d173cc41057ddca2d30
amponsemmichael <amponsem_michael@yahoo.com>
2024-07-16 14:01:24 +0000

    initial project commit

---
+++ spring-catalog-service/src/main/java/com/amponsem/spring_catalog_service/SpringCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amponsem.spring_catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ spring-catalog-service/src/test/java/com/amponsem/spring_catalog_service/SpringCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amponsem.spring_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
